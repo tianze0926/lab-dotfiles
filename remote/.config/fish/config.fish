6d5005515e1deec4e9cac29f6eb7727655ea6920
zhoutianze-i <zhoutianze-i@01.ai>
2024-09-16 20:28:43 +0800

    update

--- remote/.config/fish/config.fish
+++ remote/.config/fish/config.fish
@@ -11,21 +11,26 @@ function proxy_unset
     set -e http_proxy
     set -e https_proxy
     set -e all_proxy
+    git config --global --unset http.proxy
 end
-proxy_set
+proxy_unset
 
-set -x LANG en_US.utf-8
+set -x LANG C.UTF-8
+set -e LANGUAGE
+set -e LC_ALL
+
 set -x TMPDIR $HOME/.tmp
 set -x TMUX_TMPDIR $TMPDIR
 set -x XDG_RUNTIME_DIR $TMPDIR/xdg_runtime_dir
 
 set -x TERM xterm-256color
 
-eval $HOME/miniconda3/bin/conda "shell.fish" hook $argv | source
-conda activate utils
-set -x LD_LIBRARY_PATH $HOME/miniconda3/envs/utils/lib
+fish_add_path -P $HOME/.local/bin
+fish_add_path -P $HOME/.cargo/bin
+fish_add_path -P $HOME/.local/nvim-linux64/bin
+fish_add_path -P $HOME/.local/node-v20.12.1-linux-x64/bin
 
-set -gxp PATH $HOME/.local/bin
-set -gxp PATH $HOME/.cargo/bin
-set -gxp PATH $HOME/.local/nvim-linux64/bin
-set -gxp PATH $HOME/.local/node-v20.12.1-linux-x64/bin
+alias supervisorctl="supervisorctl -c ~/.config/supervisor/supervisord.conf"
+
+eval $HOME/miniforge3/bin/conda "shell.fish" hook $argv | source
+conda activate utils
